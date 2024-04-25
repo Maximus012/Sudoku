b8cc2870c8d2cacdb51f84cbb32a1966558a00c3
admin <mkaluzny0233@gmail.com>
2024-04-25 21:02:11 +0200

    division into files + gitignore

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from input import input
 from solving import solving
 from output import output
 
-
 if __name__ == '__main__':
     board = [[0 for a in range(9)] for b in range(9)]
     board = input(board)
