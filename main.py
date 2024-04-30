5ce5b5a57cd94dfe396f88925453936988402630
admin <mkaluzny0233@gmail.com>
2024-04-30 22:42:17 +0200

    input completed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from solving import solving
 from output import output
 
 if __name__ == '__main__':
-    board = [[0 for a in range(9)] for b in range(9)]
-    board = input(board)
+    board = input()
+    print(board)
     board = solving(board)
     board = output(board)
