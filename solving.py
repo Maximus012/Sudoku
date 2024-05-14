6f710241cdbd95842e5b20c6083be4f6d043a392
admin <mkaluzny0233@gmail.com>
2024-05-14 17:25:30 +0200

    added lines

--- solving.py
+++ solving.py
@@ -1,6 +1,4 @@
 def solving(board):
     print("Program 2")
 
-#     rozwiązywanie sudoku
-#     tutaj powinno być dużo funkcji, żeby to ładnie wyglądało, porządek etc.
-#     modularyzacja i te sprawy
+    return board
